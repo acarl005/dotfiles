a2cc6aa8a81f59c63222622dd984ff12dd2b44e8
Andrew Carlson <acarl005@g.ucla.edu>
2018-06-11 20:54:02 -0700

    add tldr to linux installs

--- linux-packages.sh
+++ linux-packages.sh
@@ -3,8 +3,10 @@ sudo apt-get install git unity-tweak-tool ubuntu-restricted-extras conky-all \
   redshift redshift-gtk default-jre xclip guake espeak exfat-fuse exfat-utils \
   traceroute ttf-ancient-fonts python3-dev libxml2-dev libxslt-dev curl \
   vim-gtk python3-pip ack-grep autojump ranger
-# TODO: install `tldr` and `fd`
+# TODO: install `fd`
 
 sudo add-apt-repository ppa:dawidd0811/neofetch
 sudo apt-get update
 sudo apt-get install neofetch
+
+pip install --user tldr
