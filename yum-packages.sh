23486546cd6ea30a884dd265d1c07c78d01dbc54
Andrew Carlson <acarl005@g.ucla.edu>
2018-06-27 17:22:29 -0700

    fix yum script

--- yum-packages.sh
+++ yum-packages.sh
@@ -1,10 +1,11 @@
-yum update -y
+sudo yum update -y
 
 sudo yum install epel-release yum-plugin-copr
 
 # add a COPR dependency
-pip install urllib3 --upgrade
+sudo pip install urllib3 --upgrade
+sudo pip install tldr
 
-yum update -y
-sudo yum install vim git tmux ack autojump neofetch curl ranger tldr
+sudo yum update -y
+sudo yum install vim git tmux ack autojump neofetch curl ranger
 
