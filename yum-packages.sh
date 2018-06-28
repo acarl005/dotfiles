42a60426def0aee794f95661a8a2b12deec01be1
Andrew Carlson <acarl005@g.ucla.edu>
2018-06-27 19:05:43 -0700

    use python package for ranger

--- yum-packages.sh
+++ yum-packages.sh
@@ -6,14 +6,10 @@ sudo yum install -y epel-release yum-plugin-copr
 sudo pip install urllib3 --upgrade
 sudo pip install tldr
 sudo pip install pygments
+sudo pip install ranger-fm
 
 yum copr -y enable konimex/neofetch
 
 sudo yum update -y
 sudo yum install -y vim git tmux ack autojump neofetch curl
-
-git clone https://github.com/ranger/ranger.git ~/src/ranger
-pushd ~/src/ranger
-sudo make install
-popd
 
