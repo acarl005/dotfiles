54e03e82a207c8355b6f4746f470d892e7c1c53f
Andrew Carlson <acarl005@g.ucla.edu>
2020-05-14 20:27:27 -0400

    improve zsh setup

--- apt-packages.sh
+++ apt-packages.sh
@@ -1,6 +1,13 @@
 sudo apt-get update -y
 
-sudo apt-get install -y git ack-grep autojump ranger tldr curl xclip shellcheck neofetch bat fzf fd-find
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# custom plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sudo apt-get install -y git ack-grep autojump ranger tldr curl xclip shellcheck neofetch bat fzf fd-find zsh
 
 if [ "$XDG_CURRENT_DESKTOP" = ubuntu:GNOME ]; then
   sudo apt-get install -y vim-gnome gnome-tweaks  
--- mac-packages.sh
+++ mac-packages.sh
@@ -1,8 +1,15 @@
-if [[ $(which brew) ]]; then
+if command -v brew >/dev/null; then
   echo Homebrew already installed
 else
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# custom plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 brew tap caskroom/fonts
 brew install ack autojump git vim coreutils neofetch bash ranger fd tldr shellcheck bat fzf onefetch
