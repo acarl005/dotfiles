05bd0988143f4141bf490b75195cf585a7d39b10
Andrew Carlson <2yinyang2@gmail.com>
2022-09-26 08:20:12 -0400

    configure telescope to open multiple files, install vim rooter, and set up diagnostics keybinds

--- init.lua
+++ init.lua
@@ -42,6 +42,9 @@ else
   vim.fn["plug#"]("universal-ctags/ctags") -- ctag extractor for all languages
   vim.fn["plug#"]("chentoast/marks.nvim") -- show the marks on the left
 
+  -- commands
+  vim.fn["plug#"]("airblade/vim-rooter") -- adds :Rooter command to change pwd to the project root
+
   -- text objects
   vim.fn["plug#"]("kana/vim-textobj-user") -- enable user-defined text objects
   vim.fn["plug#"]("glts/vim-textobj-comment") -- text object for comments to "c"
@@ -102,20 +105,111 @@ else
   require("gitsigns").setup()
   require("scrollbar").setup()
   require("marks").setup()
-  require('nvim-cursorline').setup {
+  require('nvim-cursorline').setup({
     cursorline = {
       enable = false
     }
-  }
+  })
 
   local telescope = require("telescope")
   local actions = require("telescope.actions")
+  local action_state = require('telescope.actions.state')
+  local transform_mod = require("telescope.actions.mt").transform_mod
+
+  --this is a hack to open all selected files, see this issue for source
+  --https://github.com/nvim-telescope/telescope.nvim/issues/1048
+  local function multiopen(prompt_bufnr, method)
+    local edit_file_cmd_map = {
+      vertical = "vsplit",
+      horizontal = "split",
+      tab = "tabedit",
+      default = "edit"
+    }
+    local edit_buf_cmd_map = {
+      vertical = "vert sbuffer",
+      horizontal = "sbuffer",
+      tab = "tab sbuffer",
+      default = "buffer"
+    }
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local multi_selection = picker:get_multi_selection()
+
+    if #multi_selection > 1 then
+      require("telescope.pickers").on_close_prompt(prompt_bufnr)
+      pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+      for i, entry in ipairs(multi_selection) do
+        local filename, row, col
+
+        if entry.path or entry.filename then
+          filename = entry.path or entry.filename
+
+          row = entry.row or entry.lnum
+          col = vim.F.if_nil(entry.col, 1)
+        elseif not entry.bufnr then
+          local value = entry.value
+          if not value then
+            return
+          end
+
+          if type(value) == "table" then
+            value = entry.display
+          end
+
+          local sections = vim.split(value, ":")
+
+          filename = sections[1]
+          row = tonumber(sections[2])
+          col = tonumber(sections[3])
+        end
+
+        local entry_bufnr = entry.bufnr
+
+        if entry_bufnr then
+          if not vim.api.nvim_buf_get_option(entry_bufnr, "buflisted") then
+            vim.api.nvim_buf_set_option(entry_bufnr, "buflisted", true)
+          end
+          local command = i == 1 and "buffer" or edit_buf_cmd_map[method]
+          pcall(vim.cmd, string.format("%s %s", command, vim.api.nvim_buf_get_name(entry_bufnr)))
+        else
+          local command = i == 1 and "edit" or edit_file_cmd_map[method]
+          if vim.api.nvim_buf_get_name(0) ~= filename or command ~= "edit" then
+            filename = require("plenary.path"):new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+            pcall(vim.cmd, string.format("%s %s", command, filename))
+          end
+        end
+
+        if row and col then
+          pcall(vim.api.nvim_win_set_cursor, 0, { row, col - 1 })
+        end
+      end
+    else
+      actions["select_" .. method](prompt_bufnr)
+    end
+  end
+
+  local custom_actions = transform_mod({
+    multi_selection_open_vertical = function(prompt_bufnr)
+      multiopen(prompt_bufnr, "vertical")
+    end,
+    multi_selection_open_horizontal = function(prompt_bufnr)
+      multiopen(prompt_bufnr, "horizontal")
+    end,
+    multi_selection_open_tab = function(prompt_bufnr)
+      multiopen(prompt_bufnr, "tab")
+    end,
+    multi_selection_open = function(prompt_bufnr)
+      multiopen(prompt_bufnr, "default")
+    end
+  })
+
   telescope.setup({
     defaults = {
       mappings = {
         i = {
           ["<c-j>"] = actions.move_selection_next,
-          ["<c-k>"] = actions.move_selection_previous
+          ["<c-k>"] = actions.move_selection_previous,
+          ["<c-o>"] = custom_actions.multi_selection_open
         }
       }
     },
@@ -235,6 +329,9 @@ else
   vim.g.vim_markdown_fenced_languages = { "c++=cpp", "bash=sh", "sqlx=pgsql", "sqlres=pgsql", "js=javascript" }
   vim.g.vim_markdown_folding_disabled = 1
 
+  -- airblade/vim-rooter options
+  vim.g.rooter_manual_only = 1
+
   vim.o.completeopt = "menu,menuone,noselect"
 end
 
@@ -294,7 +391,7 @@ vim.keymap.set("n", "gV", "`[v`]", { noremap = true })
 -- convenient save shortcut
 vim.keymap.set("n", "<leader>w", ":update<CR>", { noremap = true })
 -- dedent block and delete line with surrounding brackets
-vim.keymap.set("n", "<leader>x", "<i{]}dd[{dd", { noremap = true })
+vim.keymap.set("n", "<leader>x", "<i{0]}dd[{dd", { noremap = true })
 -- handy buffer navigation
 vim.keymap.set("n", "<c-h>", ":bprevious<CR>", { noremap = true })
 vim.keymap.set("n", "<c-l>", ":bnext<CR>", { noremap = true })
@@ -314,6 +411,13 @@ vim.keymap.set("n", "<c-j>", ":SwapDown<CR>", { noremap = true })
 -- key mappings for preservim/tagbar
 vim.keymap.set("n", "<leader>t", ":TagbarToggle<CR>", { noremap = true })
 
+-- key mappings for LSP diagnostics
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true })
+-- depends on Telescope
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true })
+
 
 -- open this file
 vim.api.nvim_create_user_command(
@@ -326,10 +430,16 @@ vim.api.nvim_create_user_command(
 )
 -- remove trailing whitespace
 vim.api.nvim_create_user_command("Trim", ":let _s=@/<Bar>:%s/\\s\\+$//e<Bar>:let @/=_s", {})
+
 -- convert snake_case to camelCase
 vim.api.nvim_create_user_command("Camel", "%s/\\([a-z0-9]\\)_\\([a-z0-9]\\)/\\1\\u\\2/g", {})
+
 -- print absolute file path
 vim.api.nvim_create_user_command("F", ":echo expand('%:p')", {})
+
+-- close all buffers except current
+vim.api.nvim_create_user_command("Purge", "%bd|e#", {})
+
 -- convert 4-space indentation to 2-space
 vim.api.nvim_create_user_command(
   "Dedent",
@@ -342,6 +452,16 @@ vim.api.nvim_create_user_command(
     vim.o.softtabstop = 2
     vim.o.expandtab = true
     vim.cmd("retab")
+  end,
+  {}
+)
+
+vim.api.nvim_create_user_command(
+  "StyleJS",
+  function()
+    vim.cmd(":%s/'/\"/g")
+    vim.cmd(":%s/;//g")
+    vim.cmd(":%s/\t/  /g")
   end,
   {}
 )
