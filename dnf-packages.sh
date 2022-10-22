826c9770d25b10830a94c69ebe15dafd0efbe903
Andy Carlson <andy@warp.dev>
2022-10-21 21:45:37 -0700

    remove snap installation b/c it doesnt work

--- dnf-packages.sh
+++ dnf-packages.sh
@@ -2,17 +2,12 @@
 
 sudo dnf update -y
 sudo dnf install -y 'dnf-command(copr)'
-sudo dnf install -y which man git neovim python3-neovim ripgrep autojump ranger tldr xclip snapd neofetch bat fzf fd-find zsh jq tmux
+sudo dnf install -y which man git neovim python3-neovim ripgrep autojump ranger tldr xclip neofetch bat fzf fd-find zsh jq tmux
 
 # TODO - enable when this is fixed
 # https://github.com/o2sh/onefetch/issues/830
 #sudo dnf copr enable -y varlad/onefetch
 #sudo dnf install -y onefetch 
-
-# enable "classic" containment snap support
-sudo ln -s /var/lib/snapd/snap /snap
-
-echo 'after restart, run "sudo snap install shellcheck"'
 
 mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/src"
