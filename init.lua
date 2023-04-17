a6637a443716cef070deb5e45fd01f7052ed95d8
Andy Carlson <andy@warp.dev>
2023-04-17 09:31:03 -0400

    add multi_open to telescope

--- init.lua
+++ init.lua
@@ -154,6 +154,29 @@ local config = {
                                 ensure_installed = { "lua_ls" },
                         },
                 },
+                {
+                        "nvim-telescope/telescope.nvim",
+                        opts = function(_, opts)
+                                local actions = require("telescope.actions")
+                                local action_state = require("telescope.actions.state")
+
+                                -- if picker has selected files, open those too on pressing "enter"
+                                local multi_open = function(pb)
+                                        local picker = action_state.get_current_picker(pb)
+                                        local multi = picker:get_multi_selection()
+                                        actions.select_default(pb)    -- the normal enter behaviour
+                                        for _, j in pairs(multi) do
+                                                if j.path ~= nil then -- is it a file -> open it as well:
+                                                        vim.cmd(string.format("%s %s", "edit", j.path))
+                                                end
+                                        end
+                                end
+
+                                opts.defaults.mappings.i["<cr>"] = multi_open
+                                opts.defaults.mappings.n["<cr>"] = multi_open
+                                return opts
+                        end,
+                },
                 -- custom plugins
                 {
                         "catppuccin/nvim",
