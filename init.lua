e720a38a661c1bf85c11d56ed39de15b08f0d947
Andy Carlson <andy@warp.dev>
2023-06-11 00:04:55 -0700

    add sick new text objects based on treesitter

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ local config = {
                                 opts.config.layout[4].val[7] = {
                                         val = "  Configure",
                                         opts = {
-                                                shortcut = "SPC ,",
+                                                shortcut = "SPC ,  ",
                                                 hl_shortcut = "DashboardShortcut",
                                                 align_shortcut = "right",
                                                 width = 36,
@@ -155,9 +155,107 @@ local config = {
                 },
                 {
                         "nvim-treesitter/nvim-treesitter",
+                        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
                         opts = {
                                 auto_install = vim.fn.executable "tree-sitter" == 1,
                                 ensure_installed = { "lua", "vim" },
+                                textobjects = {
+                                        select = {
+                                                enable = true,
+                                                lookahead = true,
+                                                keymaps = {
+                                                        aA = "@attribute.outer",
+                                                        iA = "@attribute.inner",
+                                                        aB = "@block.outer",
+                                                        iB = "@block.inner",
+                                                        aD = "@conditional.outer",
+                                                        iD = "@conditional.inner",
+                                                        aF = "@function.outer",
+                                                        iF = "@function.inner",
+                                                        aL = "@loop.outer",
+                                                        iL = "@loop.inner",
+                                                        aP = "@parameter.outer",
+                                                        iP = "@parameter.inner",
+                                                        aR = "@regex.outer",
+                                                        iR = "@regex.inner",
+                                                        aX = "@class.outer",
+                                                        iX = "@class.inner",
+                                                        aS = "@statement.outer",
+                                                        iS = "@statement.outer",
+                                                        aN = "@number.inner",
+                                                        iN = "@number.inner",
+                                                        aC = "@comment.outer",
+                                                        iC = "@comment.outer",
+                                                },
+                                        },
+                                        move = {
+                                                enable = true,
+                                                set_jumps = true,
+                                                goto_next_start = {
+                                                        ["]b"] = { query = "@block.outer", desc = "Next block start" },
+                                                        ["]f"] = { query = "@function.outer", desc =
+                                                        "Next function start" },
+                                                        ["]p"] = { query = "@parameter.outer", desc =
+                                                        "Next parameter start" },
+                                                        ["]x"] = { query = "@class.outer", desc = "Next class start" },
+                                                        ["]c"] = { query = "@comment.outer", desc = "Next comment start" },
+                                                },
+                                                goto_next_end = {
+                                                        ["]B"] = { query = "@block.outer", desc = "Next block end" },
+                                                        ["]F"] = { query = "@function.outer", desc = "Next function end" },
+                                                        ["]P"] = { query = "@parameter.outer", desc =
+                                                        "Next parameter end" },
+                                                        ["]X"] = { query = "@class.outer", desc = "Next class end" },
+                                                        ["]C"] = { query = "@comment.outer", desc = "Next comment end" },
+                                                },
+                                                goto_previous_start = {
+                                                        ["[b"] = { query = "@block.outer", desc = "Previous block start" },
+                                                        ["[f"] = { query = "@function.outer", desc =
+                                                        "Previous function start" },
+                                                        ["[p"] = { query = "@parameter.outer", desc =
+                                                        "Previous parameter start" },
+                                                        ["[x"] = { query = "@class.outer", desc = "Previous class start" },
+                                                        ["[c"] = { query = "@comment.outer", desc =
+                                                        "Previous comment start" },
+                                                },
+                                                goto_previous_end = {
+                                                        ["[B"] = { query = "@block.outer", desc = "Previous block end" },
+                                                        ["[F"] = { query = "@function.outer", desc =
+                                                        "Previous function end" },
+                                                        ["[P"] = { query = "@parameter.outer", desc =
+                                                        "Previous parameter end" },
+                                                        ["[X"] = { query = "@class.outer", desc = "Previous class end" },
+                                                        ["[C"] = { query = "@comment.outer", desc =
+                                                        "Previous comment end" },
+                                                },
+                                        },
+                                        swap = {
+                                                enable = true,
+                                                swap_next = {
+                                                        [">B"] = { query = "@block.outer", desc = "Swap next block" },
+                                                        [">F"] = { query = "@function.outer", desc = "Swap next function" },
+                                                        [">P"] = { query = "@parameter.inner", desc =
+                                                        "Swap next parameter" },
+                                                },
+                                                swap_previous = {
+                                                        ["<B"] = { query = "@block.outer", desc = "Swap previous block" },
+                                                        ["<F"] = { query = "@function.outer", desc =
+                                                        "Swap previous function" },
+                                                        ["<P"] = { query = "@parameter.inner", desc =
+                                                        "Swap previous parameter" },
+                                                },
+                                        },
+                                        lsp_interop = {
+                                                enable = true,
+                                                border = "single",
+                                                peek_definition_code = {
+                                                        ["<leader>lp"] = { query = "@function.outer", desc =
+                                                        "Peek function definition" },
+                                                        ["<leader>lP"] = { query = "@class.outer", desc =
+                                                        "Peek class definition" },
+                                                },
+                                        },
+                                },
                         },
                 },
                 {
@@ -191,7 +289,7 @@ local config = {
                 },
                 -- custom plugins
                 {
-                        "catppuccin/nvim",
+                        "catppuccin/nvim", -- pretty dark color scheme
                         as = "catppuccin",
                         config = function()
                                 require("catppuccin").setup()
@@ -212,7 +310,7 @@ local config = {
                         end,
                 },
                 {
-                        "chentoast/marks.nvim", -- show the marks on the left
+                        "chentoast/marks.nvim", -- show the marks in the gutter
                         lazy = false,
                         config = function()
                                 require("marks").setup()
@@ -229,11 +327,6 @@ local config = {
                         "tyru/open-browser-github.vim", -- opens current buffer in GitHub
                         lazy = false,
                         dependencies = { "tyru/open-browser.vim" },
-                },
-                {
-                        "sgur/vim-textobj-parameter", -- text object for function params to ","
-                        lazy = false,
-                        dependencies = { "kana/vim-textobj-user" },
                 },
                 {
                         "AndrewRadev/splitjoin.vim",
