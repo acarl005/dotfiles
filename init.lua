b6678627953e58395486c069d80f160f304b6c6a
Andy Carlson <andy@warp.dev>
2023-08-19 19:20:50 -0400

    remove some stuff i added in favor of default

--- init.lua
+++ init.lua
@@ -60,7 +60,7 @@ return {
 					":vsp<cr>:lua vim.lsp.buf.definition()<cr>",
 					desc = "Open definition in a vsplit",
 				},
-				["&T"] = {
+				["&y"] = {
 					":vsp<cr>:lua vim.lsp.buf.type_definition()<cr>",
 					desc = "Open type definition in a vsplit",
 				},
@@ -128,7 +128,7 @@ return {
 		{
 			"stevearc/aerial.nvim",
 			opts = {
-				disable_max_lines = 20000,
+				disable_max_lines = 32768,
 				-- 4 MiB
 				disable_max_size = 4194304,
 			},
@@ -136,6 +136,8 @@ return {
 		{
 			"rebelot/heirline.nvim",
 			opts = function(_, opts)
+				local status = require("astronvim.utils.status")
+
 				local file_path = {
 					{
 						provider = function()
@@ -144,128 +146,44 @@ return {
 					},
 					padding = { left = 1, right = 1 },
 				}
-				table.insert(opts.statusline, 4, file_path)
+
+				opts.statusline = {
+					hl = { fg = "fg", bg = "bg" },
+					status.component.mode({ mode_text = { padding = { left = 1, right = 1 } } }),
+					status.component.git_branch(),
+					status.component.file_info({ filetype = {}, filename = false, file_modified = false }),
+					file_path,
+					status.component.git_diff(),
+					status.component.diagnostics(),
+					status.component.fill(),
+					status.component.cmd_info(),
+					status.component.fill(),
+					status.component.lsp(),
+					status.component.treesitter(),
+					status.component.nav(),
+				}
+
 				return opts
 			end,
 		},
 		{
 			"nvim-treesitter/nvim-treesitter",
-			dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 			opts = {
 				auto_install = vim.fn.executable("tree-sitter") == 1,
 				ensure_installed = { "lua", "vim" },
 				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							aA = "@attribute.outer",
-							iA = "@attribute.inner",
-							aB = "@block.outer",
-							iB = "@block.inner",
-							aC = "@conditional.outer",
-							iC = "@conditional.inner",
-							aF = "@function.outer",
-							iF = "@function.inner",
-							aL = "@loop.outer",
-							iL = "@loop.inner",
-							aP = "@parameter.outer",
-							iP = "@parameter.inner",
-							aR = "@regex.outer",
-							iR = "@regex.inner",
-							aX = "@class.outer",
-							iX = "@class.inner",
-							aS = "@statement.outer",
-							iS = "@statement.outer",
-							aN = "@number.inner",
-							iN = "@number.inner",
-						},
-					},
 					move = {
-						enable = true,
-						set_jumps = true,
 						goto_next_start = {
-							["]b"] = { query = "@block.outer", desc = "Next block start" },
-							["]f"] = {
-								query = "@function.outer",
-								desc = "Next function start",
-							},
-							["]p"] = {
-								query = "@parameter.outer",
-								desc = "Next parameter start",
-							},
-							["]x"] = { query = "@class.outer", desc = "Next class start" },
-							["]c"] = {
-								query = "@conditional.outer",
-								desc = "Next conditional start",
-							},
+							["]c"] = { query = "@class.outer", desc = "Next class start" },
 						},
 						goto_next_end = {
-							["]B"] = { query = "@block.outer", desc = "Next block end" },
-							["]F"] = { query = "@function.outer", desc = "Next function end" },
-							["]P"] = {
-								query = "@parameter.outer",
-								desc = "Next parameter end",
-							},
-							["]X"] = { query = "@class.outer", desc = "Next class end" },
-							["]C"] = {
-								query = "@conditional.outer",
-								desc = "Next conditional end",
-							},
+							["]C"] = { query = "@class.outer", desc = "Next class end" },
 						},
 						goto_previous_start = {
-							["[b"] = { query = "@block.outer", desc = "Previous block start" },
-							["[f"] = {
-								query = "@function.outer",
-								desc = "Previous function start",
-							},
-							["[p"] = {
-								query = "@parameter.outer",
-								desc = "Previous parameter start",
-							},
-							["[x"] = { query = "@class.outer", desc = "Previous class start" },
-							["[c"] = {
-								query = "@conditional.outer",
-								desc = "Previous conditional start",
-							},
+							["[c"] = { query = "@class.outer", desc = "Previous class start" },
 						},
 						goto_previous_end = {
-							["[B"] = { query = "@block.outer", desc = "Previous block end" },
-							["[F"] = {
-								query = "@function.outer",
-								desc = "Previous function end",
-							},
-							["[P"] = {
-								query = "@parameter.outer",
-								desc = "Previous parameter end",
-							},
-							["[X"] = { query = "@class.outer", desc = "Previous class end" },
-							["[C"] = {
-								query = "@conditional.outer",
-								desc = "Previous conditional end",
-							},
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							[">B"] = { query = "@block.outer", desc = "Swap next block" },
-							[">F"] = { query = "@function.outer", desc = "Swap next function" },
-							[">P"] = {
-								query = "@parameter.inner",
-								desc = "Swap next parameter",
-							},
-						},
-						swap_previous = {
-							["<B"] = { query = "@block.outer", desc = "Swap previous block" },
-							["<F"] = {
-								query = "@function.outer",
-								desc = "Swap previous function",
-							},
-							["<P"] = {
-								query = "@parameter.inner",
-								desc = "Swap previous parameter",
-							},
+							["[C"] = { query = "@class.outer", desc = "Previous class end" },
 						},
 					},
 					lsp_interop = {
@@ -316,6 +234,17 @@ return {
 				return opts
 			end,
 		},
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			opts = {
+				window = {
+					mappings = {
+						H = "prev_source",
+						L = "next_source",
+					},
+				},
+			},
+		},
 		-- custom plugins
 		{
 			"AstroNvim/astrocommunity",
@@ -348,19 +277,24 @@ return {
 	-- anything that doesn't fit in the normal config locations above can go here
 	polish = function()
 		-- remove trailing whitespace
-		vim.api.nvim_create_user_command("Trim", ":let _s=@/<Bar>:%s/\\s\\+$//e<Bar>:let @/=_s", {})
+		vim.api.nvim_create_user_command(
+			"Trim",
+			":let _s=@/<Bar>:%s/\\s\\+$//e<Bar>:let @/=_s",
+			{ desc = "Trim the trailing whitespace off each line" }
+		)
 
 		-- convert snake_case to camelCase
-		vim.api.nvim_create_user_command("Camel", "%s/\\([a-z0-9]\\)_\\([a-z0-9]\\)/\\1\\u\\2/g", {})
+		vim.api.nvim_create_user_command(
+			"Camel",
+			"%s/\\([a-z0-9]\\)_\\([a-z0-9]\\)/\\1\\u\\2/g",
+			{ desc = "Convert snake case to camel case" }
+		)
 
 		-- print absolute file path
-		vim.api.nvim_create_user_command("F", ":echo expand('%:p')", {})
-
-		-- close all buffers except current
-		vim.api.nvim_create_user_command("Purge", "%bd|e#|bd#", {})
+		vim.api.nvim_create_user_command("F", ":echo expand('%:p')", { desc = "Echo absolute path of current buffer" })
 
 		-- open this file
-		vim.api.nvim_create_user_command("Conf", edit_config, {})
+		vim.api.nvim_create_user_command("Conf", edit_config, { desc = "Edit config file" })
 
 		-- shows how the current buffer differs from the file on disk
 		vim.cmd([[
@@ -373,16 +307,5 @@ return {
 				endfunc
 			com! DiffSaved call s:DiffWithSaved()
     ]])
-
-		vim.api.nvim_create_autocmd("FocusGained", {
-			callback = function()
-				vim.notify("Welcome back", nil, { timeout = 500 })
-			end,
-		})
-		vim.api.nvim_create_autocmd("FocusLost", {
-			callback = function()
-				vim.notify("Goodbye", nil, { timeout = 500 })
-			end,
-		})
 	end,
 }
