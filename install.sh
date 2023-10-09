fbab00fee625e53dc992d59a0c8b75cfe7b64ba3
Andy Carlson <andy@warp.dev>
2023-10-09 19:39:28 -0400

    fix cargo install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -e
 # install Rustup mainly for cargo packages
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 
-cargo install joshuto git-delta
+"$HOME/.cargo/bin/cargo" install joshuto git-delta
 
 if [[ $(uname) = Darwin ]]; then
   . mac-packages.sh
