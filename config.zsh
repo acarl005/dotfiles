da34da6508224d1054462983856b1aabf273e887
Andy Carlson <2yinyang2@gmail.com>
2024-01-08 10:17:17 -0500

    remove "ll" alias from oh-my-zsh

--- config.zsh
+++ config.zsh
@@ -6,6 +6,10 @@ fi
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# Skip only aliases defined in the directories.zsh lib file
+# This defines an "ll" alias which overlaps with my own
+zstyle ':omz:lib:directories' aliases no
+
 . "$ZSH/oh-my-zsh.sh"
 
 eval "$(starship init zsh)"
