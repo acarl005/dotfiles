79a53b6ba0042b195b5759d8941c70bb770e52aa
Andy Carlson <2yinyang2@gmail.com>
2024-05-16 22:22:14 +0200

    migrate diagnostic severity limit

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -34,11 +34,7 @@ vim.cmd [[
 	com! DiffSaved call s:DiffWithSaved()
 ]]
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  signs = {
-    severity_limit = "Hint",
-  },
-  virtual_text = {
-    severity_limit = "Warning",
-  },
-})
+vim.diagnostic.config {
+  virtual_text = { severity = { min = vim.diagnostic.severity.WARN } },
+  signs = { severity = { min = vim.diagnostic.severity.HINT } },
+}
