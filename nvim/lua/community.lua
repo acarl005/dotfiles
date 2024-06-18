c2a0296537a3217be862d9cdc0060076a57fb73c
Andy Carlson <andy@warp.dev>
2024-06-18 16:39:33 -0400

    add some sick plugins

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -2,15 +2,15 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.editing-support.mini-splitjoin" },
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
   { import = "astrocommunity.utility.noice-nvim" },
-  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
 }
--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -118,7 +118,34 @@ return {
     dependencies = { "kana/vim-textobj-user" },
   },
   {
-    "FabijanZulj/blame.nvim",
-    cmd = { "BlameToggle" },
+    "Saecki/crates.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          CmpSourceCargo = {
+            {
+              event = "BufRead",
+              desc = "Load crates.nvim into Cargo buffers",
+              pattern = "Cargo.toml",
+              callback = function()
+                require("cmp").setup.buffer { sources = { { name = "crates" } } }
+                require "crates"
+              end,
+            },
+          },
+        },
+      },
+    },
+    opts = {
+      completion = {
+        cmp = { enabled = true },
+      },
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
+      },
+    },
   },
 }
