e97d2deaff1a6544684338e94ff896ba61b86c01
Andy Carlson <2yinyang2@gmail.com>
2025-01-08 13:17:43 -0500

    add treesitter text objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,21 +8,33 @@ return {
       move = {
         goto_next_start = {
           ["]c"] = { query = "@class.outer", desc = "Next class start" },
+          ["]f"] = { query = "@function.outer", desc = "Next function start" },
           ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
           ["]?"] = { query = "@conditional.outer", desc = "Next conditional start" },
         },
         goto_next_end = {
           ["]C"] = { query = "@class.outer", desc = "Next class end" },
+          ["]F"] = { query = "@function.outer", desc = "Next function end" },
           ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
         },
         goto_previous_start = {
           ["[c"] = { query = "@class.outer", desc = "Previous class start" },
+          ["[f"] = { query = "@function.outer", desc = "Previous function start" },
           ["[l"] = { query = "@loop.outer", desc = "Previous loop start" },
           ["[?"] = { query = "@conditional.outer", desc = "Previous conditional start" },
         },
         goto_previous_end = {
           ["[C"] = { query = "@class.outer", desc = "Previous class end" },
+          ["[F"] = { query = "@function.outer", desc = "Previous function end" },
           ["[L"] = { query = "@loop.outer", desc = "Previous loop end" },
+        },
+      },
+      select = {
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@comment.outer",
+          ["ic"] = "@comment.inner",
         },
       },
       lsp_interop = {
