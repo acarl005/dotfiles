12d86c85517c2081f67bef2535fe6da45f0be197
Andy Carlson <2yinyang2@gmail.com>
2025-05-28 21:27:01 -0400

    make hypr config nice

--- arch/configure.sh
+++ arch/configure.sh
@@ -6,16 +6,14 @@ DIR=$(dirname $(dirname $(readlink -f $0)))
 
 $DIR/install-dotfiles.sh
 
-mkdir -p ~/.config/hypr
-ln -fs "$DIR/hypr/hyprland.conf" ~/.config/hypr/hyprland.conf
-echo "preload = $DIR/space.jpg" > ~/.config/hypr/hyprpaper.conf
-echo "wallpaper = , $DIR/space.jpg" >> ~/.config/hypr/hyprpaper.conf
+[ -L ~/.config/hypr ] && rm ~/.config/hypr
+ln -s "$DIR/hypr" ~/.config/hypr
 [ -L ~/.config/waybar ] && rm ~/.config/waybar
 ln -s "$DIR/waybar" ~/.config/waybar
 [ -L ~/.config/wofi ] && rm ~/.config/wofi
 ln -s "$DIR/wofi" ~/.config/wofi
 
-sudo pacman -Syu kitty fish neovim fastfetch man-pages man-db hyprland hyprpaper waybar egl-wayland wl-clipboard \
+sudo pacman -Syu kitty fish neovim fastfetch man-pages man-db hyprland hyprpaper hyprcursor waybar egl-wayland wl-clipboard \
   swayidle xdg-desktop-portal xdg-desktop-portal-hyprland tmux thunar tumbler firefox htop brightnessctl unzip wofi \
   ripgrep fd fzf tldr bat onefetch jq jless starship rustup git-delta ttf-inconsolata-nerd yazi mpv vlc less \
   adobe-source-han-sans-cn-fonts fcitx5 fcitx5-configtool fcitx5-chinese-addons \
@@ -42,5 +40,5 @@ cd ~/src
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S autojump
+yay -S autojump bibata-cursor-theme-bin
 
